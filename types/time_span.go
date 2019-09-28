b4d0ab40c2073de7a4007cfe9b7b481575a803b2
Markus Gilg <markusgilg@outlook.de>
2019-09-28 13:53:03 +0200

    WIP

--- types/time_span.go
+++ types/time_span.go
@@ -1,7 +1,24 @@
 package types
 
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
+var millisecondsPerSecond = NewNumber(1000)
+var hoursPerDay = NewNumber(24)
+var minutesPerDay = hoursPerDay.Multiply(*NewNumber(60))
+var secondsPerDay = minutesPerDay.Multiply(*NewNumber(60))
+var millisecondsPerDay = secondsPerDay.Multiply(*millisecondsPerSecond)
+var minutesPerHour = NewNumber(60)
+var secondsPerHour = minutesPerHour.Multiply(*NewNumber(60))
+var millisecondsPerHour = secondsPerHour.Multiply(*millisecondsPerSecond)
+var secondsPerMinute = secondsPerHour.Divide(*NewNumber(60))
+var millisecondsPerMinute = secondsPerMinute.Multiply(*millisecondsPerSecond)
+
 type TimeSpan struct {
-	value                 int64
+	value                 time.Time
 	isValid               bool
 	MillisecondsPerSecond *Number
 	HoursPerDay           *Number
@@ -15,25 +32,117 @@ type TimeSpan struct {
 	MillisecondsPerMinute *Number
 }
 
-func NewTimeSpan(hours, minutes, seconds, milliseconds Number) *TimeSpan {
-
+func EmptyTimeSpan() *TimeSpan {
+	return &TimeSpan{
+		value:                 time.Now().UTC(),
+		isValid:               false,
+		MillisecondsPerSecond: millisecondsPerSecond,
+		HoursPerDay:           hoursPerDay,
+		MinutesPerDay:         minutesPerDay,
+		SecondsPerDay:         secondsPerDay,
+		MillisecondsPerDay:    millisecondsPerDay,
+		MinutesPerHour:        minutesPerHour,
+		SecondsPerHour:        secondsPerHour,
+		MillisecondsPerHour:   millisecondsPerHour,
+		SecondsPerMinute:      secondsPerMinute,
+		MillisecondsPerMinute: millisecondsPerMinute,
+	}
 }
 
-func NewTimeSpanFromMilliseconds(milliseconds Number) *TimeSpan {}
-func NewTimeSpanFromISOString(isoString String) *TimeSpan       {}
+func NewTimeSpan(days, hours, minutes, seconds, milliseconds Number) *TimeSpan {
+	return &TimeSpan{
+		value: time.Date(0,
+			0,
+			0,
+			hours.AsInt(),
+			minutes.AsInt(),
+			seconds.AsInt(),
+			millisecondsToNanoseconds(milliseconds).AsInt(),
+			time.UTC),
+		isValid:               true,
+		MillisecondsPerSecond: millisecondsPerSecond,
+		HoursPerDay:           hoursPerDay,
+		MinutesPerDay:         minutesPerDay,
+		SecondsPerDay:         secondsPerDay,
+		MillisecondsPerDay:    millisecondsPerDay,
+		MinutesPerHour:        minutesPerHour,
+		SecondsPerHour:        secondsPerHour,
+		MillisecondsPerHour:   millisecondsPerHour,
+		SecondsPerMinute:      secondsPerMinute,
+		MillisecondsPerMinute: millisecondsPerMinute,
+	}
+}
 
-func (ts *TimeSpan) IsValid() bool     {}
-func (ts *TimeSpan) AsString() *String {}
+func NewTimeSpanFromMilliseconds(milliseconds Number) *TimeSpan {
+	return &TimeSpan{
+		value:                 time.Unix(0, millisecondsToNanoseconds(milliseconds).AsInt64()),
+		isValid:               true,
+		MillisecondsPerSecond: millisecondsPerSecond,
+		HoursPerDay:           hoursPerDay,
+		MinutesPerDay:         minutesPerDay,
+		SecondsPerDay:         secondsPerDay,
+		MillisecondsPerDay:    millisecondsPerDay,
+		MinutesPerHour:        minutesPerHour,
+		SecondsPerHour:        secondsPerHour,
+		MillisecondsPerHour:   millisecondsPerHour,
+		SecondsPerMinute:      secondsPerMinute,
+		MillisecondsPerMinute: millisecondsPerMinute,
+	}
+}
 
-func (ts *TimeSpan) Day() *Number               {}
+func NewTimeSpanFromISOString(isoString String) *TimeSpan {
+	t, err := isoStringReader(isoString)
+	if err != nil {
+		println("Error on TimeSpan NewTimeSpanFromISOString: " + err.Error())
+		return EmptyTimeSpan()
+	}
+	return &TimeSpan{
+		value:                 t,
+		isValid:               true,
+		MillisecondsPerSecond: millisecondsPerSecond,
+		HoursPerDay:           hoursPerDay,
+		MinutesPerDay:         minutesPerDay,
+		SecondsPerDay:         secondsPerDay,
+		MillisecondsPerDay:    millisecondsPerDay,
+		MinutesPerHour:        minutesPerHour,
+		SecondsPerHour:        secondsPerHour,
+		MillisecondsPerHour:   millisecondsPerHour,
+		SecondsPerMinute:      secondsPerMinute,
+		MillisecondsPerMinute: millisecondsPerMinute,
+	}
+}
+
+func (ts *TimeSpan) IsValid() bool {
+	return ts.isValid
+}
+
+func (ts *TimeSpan) AsString() *String {
+	println("missing Implementation")
+	return EmptyString()
+}
+
+func (ts *TimeSpan) Day() *Number {
+	return NewNumber(ts.value.Day())
+}
+func (ts *TimeSpan) Hour() *Number {
+	return NewNumber(ts.value.Hour())
+}
+func (ts *TimeSpan) Minute() *Number {
+	return NewNumber(ts.value.Minute())
+}
+func (ts *TimeSpan) Second() *Number {
+	return NewNumber(ts.value.Second())
+}
+func (ts *TimeSpan) Millisecond() *Number {
+	tmp := ts.value.Nanosecond() / int(time.Millisecond)
+	return NewNumber(tmp)
+}
+
+/*
 func (ts *TimeSpan) TotalDays() *Number         {}
-func (ts *TimeSpan) Hour() *Number              {}
 func (ts *TimeSpan) TotalHours() *Number        {}
-func (ts *TimeSpan) Minute() *Number            {}
 func (ts *TimeSpan) TotalMinutes() *Number      {}
-func (ts *TimeSpan) Second() *Number            {}
 func (ts *TimeSpan) TotalSeconds() *Number      {}
-func (ts *TimeSpan) Millisecond() *Number       {}
 func (ts *TimeSpan) TotalMilliseconds() *Number {}
 
 func (ts *TimeSpan) IsBefore(duration TimeSpan) bool      {}
@@ -42,3 +151,97 @@ func (ts *TimeSpan) Negate() *TimeSpan                    {}
 func (ts *TimeSpan) Add(duration TimeSpan) *TimeSpan      {}
 func (ts *TimeSpan) Subtract(duration TimeSpan) *TimeSpan {}
 func (ts *TimeSpan) Equals(duration TimeSpan) bool        {}
+*/
+
+func millisecondsToNanoseconds(milliseconds Number) *Number {
+	nanoseconds := milliseconds.
+		Multiply(*NewNumber(time.Millisecond)).
+		Multiply(*NewNumber(time.Microsecond))
+	return nanoseconds
+}
+
+func isoStringReader(value String) (time.Time, error) {
+	daySeperator := NewString(".")
+	timeSeperator := NewString(":")
+	msSeperator := NewString(" ")
+
+	isInLengthBorders := value.Length().IsAbove(*NewNumber(1)) && value.Length().IsBelow(*NewNumber(15))
+	daySeperatorExists := value.ContainsCount(*daySeperator).Equals(*NewNumber(1))
+	timeSeperatorExists := value.ContainsCount(*timeSeperator).Equals(*NewNumber(2))
+	msSeperatorExists := value.ContainsCount(*msSeperator).Equals(*NewNumber(1))
+
+	if !isInLengthBorders {
+		println("Error on TimeSpan isoStringReader: string is to short or to long")
+		return time.Now(), errors.New("string is to short or to long")
+	}
+
+	var daySplit []*String
+	if daySeperatorExists {
+		daySplit = value.Split(*daySeperator)
+	}
+	var timeSplit []*String
+	if timeSeperatorExists {
+		if daySplit != nil && len(daySplit) > 1 {
+			timeSplit = daySplit[1].Split(*timeSeperator)
+		} else {
+			timeSplit = value.Split(*timeSeperator)
+		}
+	}
+	var msSplit []*String
+	if msSeperatorExists {
+		if timeSplit != nil && len(timeSplit) > 2 {
+			msSplit = timeSplit[2].Split(*msSeperator)
+		} else {
+			msSplit = value.Split(*msSeperator)
+		}
+	}
+	println(msSplit)
+
+	day := 0
+	hour := 0
+	minute := 0
+	second := 0
+	millisecond := 0
+	if daySplit != nil && len(daySplit) > 0 {
+		day = daySplit[0].AsNumber().AsInt()
+	}
+	if timeSplit != nil && len(timeSplit) > 2 {
+		hour = timeSplit[0].AsNumber().AsInt()
+		minute = timeSplit[1].AsNumber().AsInt()
+		if msSplit == nil || len(msSplit) <= 1 {
+			second = timeSplit[2].AsNumber().AsInt()
+		}
+	}
+	if msSplit != nil && len(msSplit) > 1 {
+		second = msSplit[0].AsNumber().AsInt()
+		millisecond = msSplit[1].AsNumber().AsInt()
+	}
+
+	return NewTimeSpan(), errors.New("missing Implementation")
+}
+
+/*
+func isoStringWriter(value *TimeSpan) *String {
+	tmp := EmptyString()
+	if daySplit != nil && len(daySplit) > 1 {
+		tmp = tmp.Concat(*daySplit[0].Concat(*daySeperator))
+	} else {
+		tmp = tmp.Concat(*NewString("0").Concat(*daySeperator))
+	}
+	if timeSplit != nil && len(timeSplit) > 2 {
+		tmp = tmp.
+			Concat(*timeSplit[0].Concat(*timeSeperator).
+				Concat(*timeSplit[1].Concat(*timeSeperator).
+					Concat(*timeSplit[2])))
+	} else {
+		tmp = tmp.
+			Concat(*NewString("00").Concat(*timeSeperator).
+				Concat(*NewString("00").Concat(*timeSeperator).
+					Concat(*NewString("00"))))
+	}
+	if msSplit != nil && len(msSplit) > 1 {
+		tmp = tmp.Concat(*msSeperator).Concat(*msSplit[1])
+	}
+	return tmp
+}
+*/
---
+++ types/time_span_test.go
@@ -0,0 +1,85 @@
+package types
+
+import (
+	"github.com/onsi/gomega"
+	"testing"
+)
+
+func TestNewTimeSpan(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewTimeSpan(*NewNumber(1), *NewNumber(2), *NewNumber(5), *NewNumber(50))
+
+	g.Expect(example.IsValid()).To(gomega.BeTrue())
+	g.Expect(example.Millisecond().AsInt()).To(gomega.Equal(50))
+	g.Expect(example.Second().AsInt()).To(gomega.Equal(5))
+	g.Expect(example.Minute().AsInt()).To(gomega.Equal(2))
+	g.Expect(example.Hour().AsInt()).To(gomega.Equal(1))
+	g.Expect(example.Day().AsInt()).To(gomega.Equal(0))
+}
+
+func TestNewTimeSpanFromMilliseconds(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example1 := NewTimeSpanFromMilliseconds(*NewNumber(1000))
+	example2 := NewTimeSpanFromMilliseconds(*NewNumber(3665001))
+
+	g.Expect(example1.IsValid()).To(gomega.BeTrue())
+	g.Expect(example1.Millisecond().AsInt()).To(gomega.Equal(0))
+	g.Expect(example1.Second().AsInt()).To(gomega.Equal(1))
+	g.Expect(example1.Minute().AsInt()).To(gomega.Equal(0))
+	g.Expect(example1.Hour().AsInt()).To(gomega.Equal(0))
+	g.Expect(example1.Day().AsInt()).To(gomega.Equal(0))
+
+	g.Expect(example2.IsValid()).To(gomega.BeTrue())
+	g.Expect(example2.Millisecond().AsInt()).To(gomega.Equal(1))
+	g.Expect(example2.Second().AsInt()).To(gomega.Equal(5))
+	g.Expect(example2.Minute().AsInt()).To(gomega.Equal(1))
+	g.Expect(example2.Hour().AsInt()).To(gomega.Equal(1))
+	g.Expect(example2.Day().AsInt()).To(gomega.Equal(1))
+}
+
+func TestNewTimeSpanFromISOString(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example1 := NewTimeSpanFromISOString(*NewString("1.22:05:08 200"))
+	example2 := NewTimeSpanFromISOString(*NewString("22:05:08 200"))
+	example3 := NewTimeSpanFromISOString(*NewString("22:05:08"))
+	example4 := NewTimeSpanFromISOString(*NewString("1.22:05:08"))
+	example5 := NewTimeSpanFromISOString(*NewString("1."))
+	example6 := NewTimeSpanFromISOString(*NewString("-"))
+
+	g.Expect(example1.IsValid()).To(gomega.BeTrue())
+	g.Expect(example1.Millisecond().AsInt()).To(gomega.Equal(200))
+	g.Expect(example1.Second().AsInt()).To(gomega.Equal(8))
+	g.Expect(example1.Minute().AsInt()).To(gomega.Equal(5))
+	g.Expect(example1.Hour().AsInt()).To(gomega.Equal(22))
+	g.Expect(example1.Day().AsInt()).To(gomega.Equal(1))
+
+	g.Expect(example2.IsValid()).To(gomega.BeTrue())
+	g.Expect(example2.Millisecond().AsInt()).To(gomega.Equal(200))
+	g.Expect(example2.Second().AsInt()).To(gomega.Equal(8))
+	g.Expect(example2.Minute().AsInt()).To(gomega.Equal(5))
+	g.Expect(example2.Hour().AsInt()).To(gomega.Equal(22))
+	g.Expect(example2.Day().AsInt()).To(gomega.Equal(0))
+
+	g.Expect(example3.IsValid()).To(gomega.BeTrue())
+	g.Expect(example3.Millisecond().AsInt()).To(gomega.Equal(0))
+	g.Expect(example3.Second().AsInt()).To(gomega.Equal(8))
+	g.Expect(example3.Minute().AsInt()).To(gomega.Equal(5))
+	g.Expect(example3.Hour().AsInt()).To(gomega.Equal(22))
+	g.Expect(example3.Day().AsInt()).To(gomega.Equal(0))
+
+	g.Expect(example4.IsValid()).To(gomega.BeTrue())
+	g.Expect(example4.Millisecond().AsInt()).To(gomega.Equal(0))
+	g.Expect(example4.Second().AsInt()).To(gomega.Equal(8))
+	g.Expect(example4.Minute().AsInt()).To(gomega.Equal(5))
+	g.Expect(example4.Hour().AsInt()).To(gomega.Equal(22))
+	g.Expect(example4.Day().AsInt()).To(gomega.Equal(1))
+
+	g.Expect(example5.IsValid()).To(gomega.BeTrue())
+	g.Expect(example5.Millisecond().AsInt()).To(gomega.Equal(0))
+	g.Expect(example5.Second().AsInt()).To(gomega.Equal(0))
+	g.Expect(example5.Minute().AsInt()).To(gomega.Equal(0))
+	g.Expect(example5.Hour().AsInt()).To(gomega.Equal(0))
+	g.Expect(example5.Day().AsInt()).To(gomega.Equal(1))
+
+	g.Expect(example6.IsValid()).To(gomega.BeFalse())
+}
