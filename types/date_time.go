abce4ea1d716977c238e12373fc6fdaa8203fa15
Markus Gilg <markusgilg@outlook.de>
2019-09-29 19:30:04 +0200

    WIP DateTime

--- types/date_time.go
+++ types/date_time.go
@@ -1,21 +1,41 @@
 package types
 
+import "time"
+
 type DateTime struct {
-	day     int
-	month   int
-	year    int
-	time    *TimeSpan
-	isValid bool
+	day      int
+	month    int
+	year     int
+	time     *TimeSpan
+	location *time.Location
+	isValid  bool
 }
 
-func NewDateTime(year, month, day, hour, minute, second, millisecond Number) *DateTime {
+func EmptyDateTime() *DateTime {
+	return &DateTime{
+		day:      0,
+		month:    0,
+		year:     0,
+		time:     EmptyTimeSpan(),
+		location: time.UTC,
+		isValid:  false,
+	}
+}
+
+func NewDateTime(location String, year, month, day, hour, minute, second, millisecond Number) *DateTime {
+	loc, err := time.LoadLocation(location.value)
+	if err != nil {
+		println("Error on DateTime NewDateTime: ", err.Error())
+		return EmptyDateTime()
+	}
 	t := NewTimeSpan(*NewNumber(0), hour, minute, second, millisecond)
 	return &DateTime{
-		day:     day.AsInt(),
-		month:   month.AsInt(),
-		year:    year.AsInt(),
-		time:    t,
-		isValid: true,
+		day:      day.AsInt(),
+		month:    month.AsInt(),
+		year:     year.AsInt(),
+		time:     t,
+		location: loc,
+		isValid:  true,
 	}
 }
 
@@ -45,4 +65,125 @@ func (dt *DateTime) Second() *Number {
 
 func (dt *DateTime) Millisecond() *Number {
 	return dt.time.Millisecond()
+}
+
+func (dt *DateTime) Clone() *DateTime {
+	return NewDateTime(
+		*NewString(dt.location.String()),
+		*dt.Year(),
+		*dt.Month(),
+		*dt.Day(),
+		*dt.Hour(),
+		*dt.Minute(),
+		*dt.Second(),
+		*dt.Millisecond())
+}
+
+func (dt *DateTime) AddYears(years Number) *DateTime {
+	clone := dt.Clone()
+	clone.SetYear(years)
+	return clone
+}
+
+func (dt *DateTime) AddMonths(months Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(months, *NewNumber(12), clone.SetMonth)
+	return clone
+}
+
+func (dt *DateTime) AddDays(days Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(days, *NewNumber(30), clone.SetDay)
+	return clone
+}
+
+func (dt *DateTime) AddHours(hours Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(hours, *dt.time.HoursPerDay, clone.SetHour)
+	return clone
+}
+
+func (dt *DateTime) AddMinutes(minutes Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(minutes, *dt.time.MinutesPerDay, clone.SetMinute)
+	return clone
+}
+
+func (dt *DateTime) AddSeconds(seconds Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(seconds, *dt.time.SecondsPerMinute, clone.SetSecond)
+	return clone
+}
+
+func (dt *DateTime) AddMilliseconds(milliseconds Number) *DateTime {
+	clone := dt.Clone()
+	appendTime(milliseconds, *dt.time.MillisecondsPerSecond, clone.SetSecond)
+	return clone
+}
+
+func (dt *DateTime) SetYear(year Number) *DateTime {
+	clone := dt.Clone()
+	clone.year = year.AsInt()
+	return clone
+}
+
+func (dt *DateTime) SetMonth(month Number) *DateTime {
+	clone := dt.Clone()
+	clone.month = month.AsInt()
+	return clone
+}
+
+func (dt *DateTime) SetDay(day Number) *DateTime {
+	clone := dt.Clone()
+	clone.day = day.AsInt()
+	return clone
+}
+
+func (dt *DateTime) SetHour(hour Number) *DateTime {
+	clone := dt.Clone()
+	clone.time.hour = hour.AsInt64()
+	return clone
+}
+
+func (dt *DateTime) SetMinute(minute Number) *DateTime {
+	clone := dt.Clone()
+	clone.time.minute = minute.AsInt64()
+	return clone
+}
+
+func (dt *DateTime) SetSecond(second Number) *DateTime {
+	clone := dt.Clone()
+	clone.time.second = second.AsInt64()
+	return clone
+}
+
+func (dt *DateTime) SetMillisecond(millisecond Number) *DateTime {
+	clone := dt.Clone()
+	clone.time.millisecond = millisecond.AsInt64()
+	return clone
+}
+
+/*
+func (dt *DateTime) InZone(location String) *DateTime {
+	loc, err := time.LoadLocation(location.value)
+	if err != nil {
+		println("Error on DateTime NewDateTime: ", err.Error())
+		return EmptyDateTime()
+	}
+	clone := dt.Clone()
+	if dt.location != loc {
+		clone.
+	}
+	return clone
+}
+*/
+
+func appendTime(value, factor Number, setter func(Number) *DateTime) {
+	border := factor.Subtract(*NewNumber(1))
+	if value.IsAbove(*border) {
+		offset := value.Divide(factor).Floor(ZERO)
+		setter(*value.Subtract(*offset.Multiply(factor)))
+	} else {
+		setter(value)
+	}
 }
--- types/date_time_test.go
+++ types/date_time_test.go
@@ -8,6 +8,7 @@ import (
 func TestNewDateTime(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	example := NewDateTime(
+		*NewString("UTC"),
 		*NewNumber(2019),
 		*NewNumber(5),
 		*NewNumber(1),
@@ -29,4 +30,102 @@ func TestNewDateTime(t *testing.T) {
 		To(gomega.Equal(10))
 	g.Expect(example.Millisecond().AsInt()).
 		To(gomega.Equal(0))
+}
+
+func TestDateTime_SetYear(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetYear(*NewNumber(2)).Year().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetMonth(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetMonth(*NewNumber(2)).Month().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetDay(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetDay(*NewNumber(2)).Day().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetHour(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetHour(*NewNumber(2)).Hour().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetMinute(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetMinute(*NewNumber(2)).Minute().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetSecond(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetSecond(*NewNumber(2)).Second().AsInt()).To(gomega.Equal(2))
+}
+
+func TestDateTime_SetMillisecond(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	example := NewDateTime(
+		*NewString("UTC"),
+		*NewNumber(2019),
+		*NewNumber(5),
+		*NewNumber(1),
+		*NewNumber(22),
+		*NewNumber(45),
+		*NewNumber(10),
+		ZERO)
+	g.Expect(example.SetMillisecond(*NewNumber(2)).Millisecond().AsInt()).To(gomega.Equal(2))
 }
